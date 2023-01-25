73ce98758f18d58047b1718c27a2f11cb9ae2fec
Viktor Larionov <larionov92spb@gmail.com>
2023-01-25 11:22:02 +0300

    Почти закончили с имени в контексте буквицы. Рисуем буквицу. Осталось реализовать отрисовку связей имени

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from math import cos, sin, pi, sqrt
+font = ImageFont.truetype("Roboto-Regular.ttf", 18, encoding='UTF-8')
 alf = [str(i) for i in range(1, 49 + 1)]
+alf = list('АБВГДеЕЖSZИiЇћКЛМНОПРСТУƔФXѾЦЧШЩЪЫьҌЮЯЭѠѦѪѨѬѮѰӨѴӔ')
 # Пустой желтый фон.
-width = height = 500
+width = height = 800
 fon_color = (255, 255, 255)
 padding = 50
+text_padding = 20
 im = Image.new('RGB', (width, height), fon_color)
 draw = ImageDraw.Draw(im)
-draw.ellipse((0 + padding, 0 + padding, width - padding, height - padding), outline=(0, 0, 0), width=2)
-n = 5
+n = len(alf)
 xc = width // 2
 yc = height // 2
-r = (width // 2) - padding + 10
+r_point = (width // 2) - padding
+r_text = (width // 2) - padding + text_padding
+fi = -pi/2
 points = []
-fi = pi/n
 # запоминаем точки многоугольника на окружности
 for i in range(n):
-    xi = xc + r * cos(fi + (2 * pi * i) / (n))
-    yi = yc + r * sin(fi + (2 * pi * i) / (n))
-    points.append((xi, yi))
-for i in range(n):
-    draw.line((points[i - 1], points[i]), fill='blue', width=2)
+    x_p = xc + r_point * cos(fi + (2 * pi * i) / (n))
+    y_p = yc + r_point * sin(fi + (2 * pi * i) / (n))
+    x_t = xc + r_text * cos(fi + (2 * pi * i) / (n))
+    y_t = yc + r_text * sin(fi + (2 * pi * i) / (n))
+    points.append((x_p, y_p))
+
     draw.text(
-        (points[i][0]-5,points[i][1]-5),
+        (x_t-5,y_t-8),
         alf[i],
-        fill=('#1C0606')
+        fill=('#000'),
+        font=font
     )
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        draw.line((points[i], points[j]), fill='#eee8aa', width=1)
+    x,y = points[i]
+    draw.ellipse((x-2,y-2,x+2,y+2),"#000")
+
 im.show()
